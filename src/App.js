8533ea18dae812c90ef1cbd156af71ff1145ebae
Karannshah1 <80153010+Karannshah1@users.noreply.github.com>
2025-10-07 10:39:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ import Register from './components/Register';
 import Sender from './components/Sender';
 import Dashboard from './components/Dashboard';
 
+const url = process.env.REACT_APP_API_URL; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+
 function App() {
   return (
     <Router>
